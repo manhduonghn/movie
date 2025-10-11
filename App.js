bbf714940b14a2cb8e160a84a5b7e89116f7474a
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2025-10-11 13:11:52 +0700

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import HomeScreen from './HomeScreen';
-import DetailScreen from './DetailScreen';
+import DetailScreen from './DetailScreen'; 
 
 const Stack = createNativeStackNavigator();
 
@@ -16,23 +16,22 @@ export default function App() {
         initialRouteName="Home"
         screenOptions={{
           headerStyle: {
-            backgroundColor: '#1E1E1E', // Màu nền header
+            backgroundColor: '#1E1E1E',
           },
-          headerTintColor: '#FFD700', // Màu chữ header (vàng)
-          headerBackTitleVisible: false, // Ẩn chữ "Back" trên iOS
+          headerTintColor: '#FFD700',
+          headerBackTitleVisible: false,
         }}
       >
         <Stack.Screen
           name="Home"
           component={HomeScreen}
           options={{
-            headerShown: false, // Ẩn header trên màn hình Home
+            headerShown: false, // Ẩn header để có không gian cho thanh tìm kiếm tùy chỉnh
           }}
         />
         <Stack.Screen
           name="Detail"
           component={DetailScreen}
-          // Lấy tên phim từ params để đặt tiêu đề
           options={({ route }) => ({ title: route.params.movieName })}
         />
       </Stack.Navigator>
